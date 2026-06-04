8f2e8f8964037605acbcb05dc6d9283fc81e6e75
Admin1 <admin1@example.com>
2026-06-04 20:32:52 +1000

    Update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "my_bucket-i" {
   bucket = "my-unique-bucket-name-${random_string.bucket_suffix.result}"
 }
 
